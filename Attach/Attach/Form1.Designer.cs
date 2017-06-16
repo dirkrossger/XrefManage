40ad0a602e07c32ac4d457d6bd97ab1bfc5bcc0f
dirkrossger <dirk.rossger@afconsult.com>
2017-06-16 08:55:18 +0200

    Attach works with unopen Drawing

--- Attach/Attach/Form1.Designer.cs
+++ Attach/Attach/Form1.Designer.cs
@@ -29,11 +29,14 @@
         private void InitializeComponent()
         {
             this.button1_Add = new System.Windows.Forms.Button();
+            this.DwgListview = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1_Add
             // 
-            this.button1_Add.Location = new System.Drawing.Point(228, 12);
+            this.button1_Add.Location = new System.Drawing.Point(269, 32);
             this.button1_Add.Name = "button1_Add";
             this.button1_Add.Size = new System.Drawing.Size(75, 23);
             this.button1_Add.TabIndex = 0;
@@ -41,14 +44,46 @@
             this.button1_Add.UseVisualStyleBackColor = true;
             this.button1_Add.Click += new System.EventHandler(this.button1_Add_Click);
             // 
+            // DwgListview
+            // 
+            this.DwgListview.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DwgListview.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.DwgListview.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.DwgListview.HideSelection = false;
+            this.DwgListview.LabelWrap = false;
+            this.DwgListview.Location = new System.Drawing.Point(12, 32);
+            this.DwgListview.Name = "DwgListview";
+            this.DwgListview.Size = new System.Drawing.Size(238, 147);
+            this.DwgListview.TabIndex = 3;
+            this.DwgListview.UseCompatibleStateImageBehavior = false;
+            this.DwgListview.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Width = 117;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Drawing names";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(326, 61);
+            this.ClientSize = new System.Drawing.Size(356, 191);
+            this.Controls.Add(this.DwgListview);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1_Add);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -56,5 +91,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1_Add;
+        private System.Windows.Forms.ListView DwgListview;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Attach/Attach/Form1.cs
+++ Attach/Attach/Form1.cs
@@ -28,6 +28,8 @@ namespace Attach
         }
 
         private string[] Files;
+        private List<string> RoList;
+        private static DocumentCollection DocCol;
 
         private void button1_Add_Click(object sender, EventArgs e)
         {
@@ -40,41 +42,147 @@ namespace Attach
             Attach();
         }
 
+        private Document GetDocumentFrom(DocumentCollection docCol, string dwgPath)
+        {
+            Document Doc = null;
+            foreach (Document doc in docCol)
+            {
+                if (string.Compare(dwgPath, doc.Name, true) == 0)
+                    Doc = doc;
+            }
+            return Doc;
+        }
+
+
         void Attach()
         {
             // Get the current database and start a transaction
             Database db;
             db = AcadApp.DocumentManager.MdiActiveDocument.Database;
 
-            using (Transaction acTrans = db.TransactionManager.StartTransaction())
+            foreach (ListViewItem lvi in DwgListview.SelectedItems)
             {
-
-                foreach (string file in Files)
+                using (DocumentLock DocLock = DocCol.MdiActiveDocument.LockDocument())
                 {
-                    ObjectId acXrefId = db.AttachXref(file, Path.GetFileName(file));
-
-                    // If a valid reference is created then continue
-                    if (!acXrefId.IsNull)
+                    Database Db;
+                    Document OpenDoc = null;
+                    DocumentLock tempLock = null;
+                    string DwgName = lvi.Text;// Path.GetFileName(lvi.Text);
+                    OpenDoc = GetDocumentFrom(DocCol, DwgName);
+                    bool DocInEditor = (OpenDoc != null);
+                    if (DocInEditor)
                     {
-                        // Attach the DWG reference to the current space
-                        Point3d insPt = new Point3d(1, 1, 0);
-                        using (BlockReference acBlkRef = new BlockReference(insPt, acXrefId))
-                        {
-                            BlockTableRecord acBlkTblRec;
-                            acBlkTblRec = acTrans.GetObject(db.CurrentSpaceId, OpenMode.ForWrite) as BlockTableRecord;
+                        Db = OpenDoc.Database;
+                        tempLock = OpenDoc.LockDocument();
+                    }
+                    else
+                        Db = new Database(true, false);
 
-                            acBlkTblRec.AppendEntity(acBlkRef);
-                            acTrans.AddNewlyCreatedDBObject(acBlkRef, true);
+                    bool saveRequired = false;
+                    using (Transaction acTrans = Db.TransactionManager.StartTransaction())
+                    {
+
+                        foreach (string file in Files)
+                        {
+                            ObjectId acXrefId = Db.AttachXref(file, Path.GetFileName(file));
+
+                            // If a valid reference is created then continue
+                            if (!acXrefId.IsNull)
+                            {
+                                // Attach the DWG reference to the current space
+                                Point3d insPt = new Point3d(0, 0, 0);
+                                using (BlockReference acBlkRef = new BlockReference(insPt, acXrefId))
+                                {
+                                    BlockTableRecord acBlkTblRec;
+                                    acBlkTblRec = acTrans.GetObject(Db.CurrentSpaceId, OpenMode.ForWrite) as BlockTableRecord;
+
+                                    acBlkTblRec.AppendEntity(acBlkRef);
+                                    acTrans.AddNewlyCreatedDBObject(acBlkRef, true);
+
+                                    saveRequired = true;
+                                }
+                            }
                         }
+                        if (saveRequired)
+                            Db.SaveAs(@"C:\Users\A480836\Google Drive\XrefManage\1.dwg", DwgVersion.Current);
+
+
+                        // Save the new objects to the database
+                        acTrans.Commit();
+
+                        // Dispose of the transaction
                     }
                 }
-
-
-                // Save the new objects to the database
-                acTrans.Commit();
-
-                // Dispose of the transaction
             }
+        }
+
+        void ListFiles()
+        {
+            RoList = new List<string>();
+            DocCol = AcadApp.DocumentManager;
+
+            foreach (string DwgPath in Files)
+            {
+                if ((File.GetAttributes(DwgPath) & FileAttributes.ReadOnly) == FileAttributes.ReadOnly)
+                {
+                    RoList.Add(DwgPath);
+                    continue;
+                }
+                string[] tempStrArray = DwgPath.Split('\\');
+                tempStrArray = tempStrArray[tempStrArray.Length - 1].Split('.');
+                ListViewItem lvi = new ListViewItem(tempStrArray[0]);
+                Database Db;
+                DocumentLock tempLock = null;
+                Document OpenDoc = null;
+                OpenDoc = GetDocumentFrom(DocCol, DwgPath);
+                bool DocInEditor = (OpenDoc != null);
+                if (DocInEditor)
+                {
+                    Db = OpenDoc.Database;
+                    tempLock = OpenDoc.LockDocument();
+                    Db.ResolveXrefs(false, true);
+                }
+                else
+                    Db = new Database(true, false);
+                try
+                {
+                    if (!DocInEditor)
+                        Db.ReadDwgFile(DwgPath, System.IO.FileShare.ReadWrite, true, null);
+
+                    DwgListview.Items.Add(lvi);
+                }
+                catch (Autodesk.AutoCAD.Runtime.Exception AcadEx)
+                {
+                    MessageBox.Show(AcadEx.Message + "\n\n" + DwgPath + "\n\n" + AcadEx.StackTrace, "AutoCAD error.");
+                }
+                catch (System.Exception SysEx)
+                {
+                    MessageBox.Show(SysEx.Message + System.Environment.NewLine + System.Environment.NewLine + SysEx.StackTrace, "System error.");
+                }
+                if (DocInEditor)
+                    tempLock.Dispose();
+                else
+                    Db.Dispose();
+            }
+            StringBuilder Sb = new StringBuilder();
+            foreach (string str in RoList)
+            {
+                Sb.AppendLine(str);
+            }
+            if (!string.IsNullOrEmpty(Sb.ToString()))
+                MessageBox.Show(Sb.ToString(), "Read-only files not able to update.");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Autodesk.AutoCAD.Windows.OpenFileDialog Dia = 
+                new Autodesk.AutoCAD.Windows.OpenFileDialog("Select drawings to manage xref.", "", "dwg", "", Autodesk.AutoCAD.Windows.OpenFileDialog.OpenFileDialogFlags.AllowMultiple);
+
+            if (Dia.ShowDialog() != DialogResult.OK) return;
+            Files = Dia.GetFilenames();
+
+            ListFiles();
+
         }
     }
 }
