bef142b2aa47a42618da53a8ceba4515478b4eeb
dirk.roessger@gmail.com <dirk.roessger@gmail.com>
2017-06-19 20:22:29 +0200

    new version of Detach(remove xref from xrefviewlist and update dwglistview)

--- XrefAdd/XrefAdd/Form1.cs
+++ XrefAdd/XrefAdd/Form1.cs
@@ -23,18 +23,20 @@ namespace XrefAdd
 {
     public partial class Form1 : Form
     {
-        private static List<MyXrefInformation> XrInfoList;
+        //private static List<MyXrefInformation> XrInfoList;
         private static DocumentCollection DocCol;
         private List<string> RoList;
         private string[] Files, AttFiles;
         private string DwgPathName;
+
+        public MyXrefInformation[] XrInfo;
+
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        public MyXrefInformation[] XrInfo;
         void Form1_Load(object sender, EventArgs e)
         {
             
@@ -86,7 +88,7 @@ namespace XrefAdd
                 {
                     if (!DocInEditor)
                         Db.ReadDwgFile(DwgPath, System.IO.FileShare.ReadWrite, true, null);
-                    XrInfo = Utility.FindXrefs(Db);
+                    XrInfo = MyXrefInformation.FindXrefs(Db);
                     lvi.Tag = XrInfo;
                     if (!XrInfo.Equals(0))
                         DwgListview.Items.Add(lvi);
@@ -113,6 +115,67 @@ namespace XrefAdd
                 MessageBox.Show(Sb.ToString(), "Read-only files not able to update.");
 
         }
+
+        void ListFile(string DwgPath)
+        {
+            RoList = new List<string>();
+
+            if ((File.GetAttributes(DwgPath) & FileAttributes.ReadOnly) == FileAttributes.ReadOnly)
+                {
+                    RoList.Add(DwgPath);
+                }
+                string[] tempStrArray = DwgPath.Split('\\');
+                tempStrArray = tempStrArray[tempStrArray.Length - 1].Split('.');
+                ListViewItem lvi = new ListViewItem(tempStrArray[0]);
+                Database Db;
+                DocumentLock tempLock = null;
+                Document OpenDoc = null;
+                OpenDoc = GetDocumentFrom(DocCol, DwgPath);
+                bool DocInEditor = (OpenDoc != null);
+                if (DocInEditor)
+                {
+                    Db = OpenDoc.Database;
+                    tempLock = OpenDoc.LockDocument();
+                    Db.ResolveXrefs(false, true);
+                }
+                else
+                    Db = new Database(true, false);
+                try
+                {
+                    if (!DocInEditor)
+                        Db.ReadDwgFile(DwgPath, System.IO.FileShare.ReadWrite, true, null);
+                    XrInfo = MyXrefInformation.FindXrefs(Db);
+                    lvi.Tag = XrInfo;
+                if (!XrInfo.Equals(0))
+                {
+                    DwgListview.Items[0].Remove();
+                    DwgListview.Items.Add(lvi);
+                }
+                   
+                }
+                catch (Autodesk.AutoCAD.Runtime.Exception AcadEx)
+                {
+                    MessageBox.Show(AcadEx.Message + "\n\n" + DwgPath + "\n\n" + AcadEx.StackTrace, "AutoCAD error.");
+                }
+                catch (System.Exception SysEx)
+                {
+                    MessageBox.Show(SysEx.Message + System.Environment.NewLine + System.Environment.NewLine + SysEx.StackTrace, "System error.");
+                }
+                if (DocInEditor)
+                    tempLock.Dispose();
+                else
+                    Db.Dispose();
+
+            StringBuilder Sb = new StringBuilder();
+            foreach (string str in RoList)
+            {
+                Sb.AppendLine(str);
+            }
+            if (!string.IsNullOrEmpty(Sb.ToString()))
+                MessageBox.Show(Sb.ToString(), "Read-only files not able to update."); 
+
+        }
+
 
         private Document GetDocumentFrom(DocumentCollection docCol, string dwgPath)
         {
@@ -199,7 +262,7 @@ namespace XrefAdd
         private void button2_Detach_Click(object sender, EventArgs e)
         {
             DocCol = AcadApp.DocumentManager;
-            XrInfoList = new List<MyXrefInformation>();
+            //XrInfoList = new List<MyXrefInformation>();
             int index = DwgListview.SelectedIndices[0];
 
             //List<ListViewItem> LocList;
@@ -209,13 +272,13 @@ namespace XrefAdd
                 {
                     foreach (MyXrefInformation xrInfoAr in lvi.Tag as MyXrefInformation[])
                     {
-
                         string DwgName = xrInfoAr.DrawingPath;
                         Database Db;
                         Document OpenDoc = null;
                         DocumentLock tempLock = null;
                         //bool ShouldSave = false;
                         OpenDoc = GetDocumentFrom(DocCol, DwgName);
+                       
                         bool DocInEditor = (OpenDoc != null);
                         if (DocInEditor)
                         {
@@ -229,6 +292,9 @@ namespace XrefAdd
                         {
                             try
                             {
+                                //for(int idx = 0; idx < XrefListview.SelectedItems.Count; idx++)
+                                //{
+                                //ListViewItem xr = XrefListview.SelectedItems[idx];
                                 foreach (ListViewItem xr in XrefListview.SelectedItems)
                                 {
                                     if (xrInfoAr.Name == xr.Text) // !!! error compare filename from XrefListview_Listbox wrong !!!
@@ -251,6 +317,7 @@ namespace XrefAdd
                                             Db.SaveAs(DwgName, DwgVersion.Current);
 
                                             xr.Remove();
+                                            ListFile(DwgName);
 
                                             break;
                                         }
@@ -277,12 +344,12 @@ namespace XrefAdd
                 }
             }
 
-            foreach (ListViewItem lvi in DwgListview.Items)
-            {
-                DwgListview.Items.Remove(lvi);
-            }
-            ListFiles(Files);
-            DwgListview.Items[index].Selected = true;
+            //foreach (ListViewItem lvi in DwgListview.Items)
+            //{
+            //    DwgListview.Items.Remove(lvi);
+            //}
+            //ListFiles(Files);
+            //DwgListview.Items[index].Selected = true;
         }
 
         private void button1_Attach_Click(object sender, EventArgs e)
@@ -349,6 +416,7 @@ namespace XrefAdd
                     }
                     if (saveRequired)
                         Db.SaveAs(DwgPathName, DwgVersion.Current);
+                    
 
 
                     // Save the new objects to the database
@@ -372,7 +440,7 @@ namespace XrefAdd
         public void Main()
         {
             DocCol = AcadApp.DocumentManager;
-            XrInfoList = new List<MyXrefInformation>();
+            //XrInfoList = new List<MyXrefInformation>();
             bool DocInEditor;
             Database Db;
 
--- XrefAdd/XrefAdd/MyXrefInformation.cs
+++ XrefAdd/XrefAdd/MyXrefInformation.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
 
 #region Autodesk
 using Autodesk.AutoCAD.ApplicationServices;
@@ -28,6 +34,8 @@ namespace XrefAdd
         private ObjectId xId;
 
         public MyXrefInformation() { }
+
+        ~MyXrefInformation() { }
 
         public string Name
         {
@@ -89,7 +97,6 @@ namespace XrefAdd
             get { return _ChildrenNames; }
             set { _ChildrenNames = value; }
         }
-
         public ObjectId XId
         {
             get
@@ -102,5 +109,219 @@ namespace XrefAdd
                 xId = value;
             }
         }
+
+        public void Remove()
+        {
+            this.XId = ObjectId.Null;
+        }
+        public static MyXrefInformation[] FindXrefs(Database db)
+        {
+            MyXrefInformation[] XrefArray;
+            string[] tempStrArray;
+            int tempCnt;
+            using (Transaction Trans = db.TransactionManager.StartTransaction())
+            {
+                BlockTable BlkTbl = (BlockTable)Trans.GetObject(db.BlockTableId, OpenMode.ForRead);
+                //db.ResolveXrefs(false, true);
+                XrefGraph XrGph = db.GetHostDwgXrefGraph(false);
+                XrefArray = new MyXrefInformation[XrGph.NumNodes - 1];
+                for (int i = 1; i < XrGph.NumNodes; ++i)
+                {
+                    XrefGraphNode XrNode = XrGph.GetXrefNode(i);
+                    BlockTableRecord btr = (BlockTableRecord)Trans.GetObject(XrNode.BlockTableRecordId, OpenMode.ForRead);
+                    MyXrefInformation XrInfo = new MyXrefInformation();
+                    XrInfo.Name = XrNode.Name;
+                    XrInfo.NewName = XrNode.Name;
+                    XrInfo.Path = btr.PathName;
+                    XrInfo.NewPath = btr.PathName;
+                    XrInfo.DrawingPath = db.Filename;
+                    XrInfo.XId = XrNode.BlockTableRecordId;
+
+                    string FoundAt = WillLoad(btr.PathName, db);
+                    if (XrNode.XrefStatus == XrefStatus.Unresolved)
+                    {
+                        if (string.IsNullOrEmpty(FoundAt))
+                            XrInfo.Status = XrefStatus.Unresolved;
+                        else
+                            XrInfo.Status = XrefStatus.Resolved;
+                    }
+                    else
+                        XrInfo.Status = XrNode.XrefStatus;
+                    if (XrInfo.Status == XrefStatus.Resolved)
+                    {
+                        XrInfo.FoundAtPath = FoundAt;
+                    }
+                    XrInfo.IsNested = XrNode.IsNested;
+                    XrInfo.IsOverlay = btr.IsFromOverlayReference;
+                    ObjectIdCollection ObjIdCol = (ObjectIdCollection)btr.GetBlockReferenceIds(true, true);
+                    string[] InsertedAtArray = new string[ObjIdCol.Count];
+                    for (int j = 0; j < ObjIdCol.Count; ++j)
+                    {
+                        ObjectId ObjId = ObjIdCol[j];
+                        BlockReference BlkRef = (BlockReference)Trans.GetObject(ObjId, OpenMode.ForRead);
+                        BlockTableRecord tempbtr = (BlockTableRecord)Trans.GetObject(BlkRef.OwnerId, OpenMode.ForRead);
+                        if (tempbtr.IsLayout)
+                        {
+                            Layout templo = (Layout)Trans.GetObject(tempbtr.LayoutId, OpenMode.ForRead);
+                            InsertedAtArray[j] = "Layout: " + templo.LayoutName;
+                        }
+                        else InsertedAtArray[j] = "Block: " + tempbtr.Name;
+                    }
+                    XrInfo.InsertedWhere = InsertedAtArray;
+                    if (!XrNode.NumIn.Equals(0))
+                    {
+                        tempStrArray = new string[XrNode.NumIn];
+                        tempCnt = 0;
+                        for (int j = 0; j < XrNode.NumIn; j++)
+                        {
+                            int tempInt = FindGraphLocation(XrNode.In(j));
+                            if (tempInt.Equals(-1))
+                                continue;
+                            tempStrArray[tempCnt] = XrGph.GetXrefNode(tempInt).Name;
+                            tempCnt++;
+                        }
+                        XrInfo.OwnerNames = tempStrArray;
+                    }
+                    if (!XrNode.NumOut.Equals(0))
+                    {
+                        tempStrArray = new string[XrNode.NumOut];
+                        tempCnt = 0;
+                        for (int j = 0; j < XrNode.NumOut; j++)
+                        {
+                            int tempInt = FindGraphLocation(XrNode.Out(j));
+                            if (tempInt.Equals(-1))
+                                continue;
+                            tempStrArray[tempCnt] = XrGph.GetXrefNode(tempInt).Name;
+                            tempCnt++;
+                        }
+                        XrInfo.ChildrenNames = tempStrArray;
+                    }
+                    XrefArray[i - 1] = XrInfo;
+                }
+            }
+            return XrefArray;
+        }
+
+        public static int FindGraphLocation(GraphNode grNode)
+        {
+            Graph Gr = grNode.Owner;
+            for (int i = 0; i < Gr.NumNodes; i++)
+            {
+                if (grNode.Equals(Gr.Node(i)))
+                    return i;
+            }
+            return -1;
+        }
+
+        public static string WillLoad(string FilePath, Database db)
+        {
+            string FoundAt = "";
+            string[] tempStrAr = FilePath.Split('.');
+            string FileExt = tempStrAr[tempStrAr.Length - 1];
+            try
+            {
+                FoundAt = HostApplicationServices.Current.FindFile(FilePath, db, FindFileHint.Default);
+            }
+            catch { }
+
+            if (!string.Compare(FoundAt, string.Empty).Equals(0))
+                return FoundAt;
+
+            if (string.Compare(FilePath.Substring(0, 2), "..").Equals(0) || string.Compare(FilePath.Substring(0, 1), ".").Equals(0))
+            {
+                string[] XrPathArray = FilePath.Split('\\');
+                string PartialPath = "";
+                for (int i = 1; i < XrPathArray.Length; ++i)
+                {
+                    PartialPath = PartialPath + "\\" + XrPathArray[i];
+                }
+                FileInfo DwgInfo = new FileInfo(db.Filename);
+                string tempFilePath = DwgInfo.DirectoryName + PartialPath;
+                try { FoundAt = HostApplicationServices.Current.FindFile(tempFilePath, db, FindFileHint.Default); }
+                catch { }
+                if (!string.Compare(FoundAt, string.Empty).Equals(0))
+                    return FoundAt;
+            }
+            return string.Empty;
+        }
+
+        public static ObjectId InsertBlock(Database db, string loName, string blkName, Point3d insPt)
+        {
+            ObjectId RtnObjId = ObjectId.Null;
+            using (Transaction Trans = db.TransactionManager.StartTransaction())
+            {
+                DBDictionary LoDict = Trans.GetObject(db.LayoutDictionaryId, OpenMode.ForRead) as DBDictionary;
+                foreach (DictionaryEntry de in LoDict)
+                {
+                    if (string.Compare((string)de.Key, loName, true).Equals(0))
+                    {
+                        Layout Lo = Trans.GetObject((ObjectId)de.Value, OpenMode.ForWrite) as Layout;
+                        BlockTable BlkTbl = Trans.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                        BlockTableRecord LoRec = Trans.GetObject(Lo.BlockTableRecordId, OpenMode.ForRead) as BlockTableRecord;
+                        ObjectId BlkTblRecId = GetNonErasedTableRecordId(BlkTbl.Id, blkName);
+                        if (BlkTblRecId.IsNull)
+                        {
+                            string BlkPath = HostApplicationServices.Current.FindFile(blkName + ".dwg", db, FindFileHint.Default);
+                            if (string.IsNullOrEmpty(BlkPath))
+                                return RtnObjId;
+                            BlkTbl.UpgradeOpen();
+                            using (Database tempDb = new Database(false, true))
+                            {
+                                tempDb.ReadDwgFile(BlkPath, FileShare.Read, true, null);
+                                db.Insert(blkName, tempDb, false);
+                            }
+                            BlkTblRecId = GetNonErasedTableRecordId(BlkTbl.Id, blkName);
+                        }
+                        LoRec.UpgradeOpen();
+                        BlockReference BlkRef = new BlockReference(insPt, BlkTblRecId);
+                        LoRec.AppendEntity(BlkRef);
+                        Trans.AddNewlyCreatedDBObject(BlkRef, true);
+                        BlockTableRecord BlkTblRec = Trans.GetObject(BlkTblRecId, OpenMode.ForRead) as BlockTableRecord;
+                        if (BlkTblRec.HasAttributeDefinitions)
+                        {
+                            foreach (ObjectId objId in BlkTblRec)
+                            {
+                                AttributeDefinition AttDef = Trans.GetObject(objId, OpenMode.ForRead) as AttributeDefinition;
+                                if (AttDef != null)
+                                {
+                                    AttributeReference AttRef = new AttributeReference();
+                                    AttRef.SetAttributeFromBlock(AttDef, BlkRef.BlockTransform);
+                                    BlkRef.AttributeCollection.AppendAttribute(AttRef);
+                                    Trans.AddNewlyCreatedDBObject(AttRef, true);
+                                }
+                            }
+                        }
+                        Trans.Commit();
+                    }
+                }
+            }
+            return RtnObjId;
+        }
+
+        public static ObjectId GetNonErasedTableRecordId(ObjectId TableId, string Name)
+        {
+            ObjectId id = ObjectId.Null;
+            using (Transaction tr = TableId.Database.TransactionManager.StartTransaction())
+            {
+                SymbolTable table = (SymbolTable)tr.GetObject(TableId, OpenMode.ForRead);
+                if (table.Has(Name))
+                {
+                    id = table[Name];
+                    if (!id.IsErased)
+                        return id;
+                    foreach (ObjectId recId in table)
+                    {
+                        if (!recId.IsErased)
+                        {
+                            SymbolTableRecord rec = (SymbolTableRecord)tr.GetObject(recId, OpenMode.ForRead);
+                            if (string.Compare(rec.Name, Name, true) == 0)
+                                return recId;
+                        }
+                    }
+                }
+            }
+            return id;
+        }
+
     }
 }
